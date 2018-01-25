a0fe449cfccd11fbd7f6160807a24d1a448bbae5
minhdang900 <minhdangcntt@gmail.com>
2018-01-25 10:05:31 +0700

    Initial config project HongDucHospital mobile version

---
+++ config/passport.js
@@ -0,0 +1,78 @@
+// config/passport.js
+
+// load all the things we need
+var LocalStrategy = require('passport-local').Strategy;
+var common = require('../common');
+var request = require('request');
+var user = {};
+// expose this function to our app using module.exports
+module.exports = function(passport) {
+
+	// =========================================================================
+	// passport session setup ==================================================
+	// =========================================================================
+	// required for persistent login sessions
+	// passport needs ability to serialize and unserialize users out of session
+
+	// used to serialize the user for the session
+	passport.serializeUser(function(user, done) {
+		done(null, user.username);
+		console.dir('serializeUser ' + user);
+	});
+
+	// used to deserialize the user
+	passport.deserializeUser(function(username, done) {
+		common.log('deserializeUser ' + username);
+		// find user by username, receive data from database
+		done(null, user);
+	});
+	// =========================================================================
+	// LOCAL LOGIN =============================================================
+	// =========================================================================
+	// we are using named strategies since we have one for login and one for
+	// signup
+	// by default, if there was no name, it would just be called 'local'
+
+	passport.use('local-login', new LocalStrategy({
+		// by default, local strategy uses username and password, we will
+		// override with email
+		usernameField : 'username',
+		passwordField : 'password',
+		passReqToCallback : true
+	// allows us to pass back the entire request to the callback
+	}, function(req, username, password, done) { // callback with username
+													// and password from our
+													// form
+//		common.log('Loggin with username ' + username + ' password:' + password);
+//		if(username == "hontam" && password == "321"){
+//			user = {
+//					username : 'hontam',
+//					fullname : 'MerPerle HonTam'//response.fullname,
+//				}
+//			return done(null, user);
+//		} else {
+//			return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+//		}
+		
+		request.post('http://hongduc.e-smile.vn:3000/hongduc/login', {form: {username: username, password: password}}, 
+				function(error, response, body){
+					if(error){
+						return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+					} else {
+						common.log(body);
+						var data = JSON.parse(body);
+						if(data.status == 1){
+							user = {
+									username : username,
+									fullname : data.message,
+								}
+								return done(null, user);
+						} else {
+							return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+						}
+						
+					}
+	    		});
+	}));
+
+};
